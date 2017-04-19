93cf077f03fd877879a777b697490af42c23b450
Daniel Arteaga <darteaga@us.es>
2017-04-19 11:46:39 +0200

    fixed some bug

--- prototype/papamoscas-icomot/papamoscas-deployment-files/papamoscas-proxy-sla/deploy.sh
+++ prototype/papamoscas-icomot/papamoscas-deployment-files/papamoscas-proxy-sla/deploy.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
+export NODE_ENV=production
 
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs nodejs-legacy
+sudo apt-get install -y nodejs
+sudo apt-get install -y nodejs-legacy
 sudo apt-get install -y build-essential
 
 #git clone https://github.com/isa-group/governify-elasticity-test-sla-proxy.git
@@ -10,7 +12,7 @@ cd governify-elasticity-test-sla-proxy
 
 npm install
 
-npm start &
+npm start &> /tmp/proxy-sla.log &
 
 cd ..
 
