6ff3b0521193109cda2a0012d9dab940d6f29e51
Daniel Arteaga <darteaga@us.es>
2017-04-21 11:32:51 +0200

    new version of deploy.sh for sla-proxy

--- prototype/papamoscas-icomot/papamoscas-deployment-files/papamoscas-proxy-sla/deploy.sh
+++ prototype/papamoscas-icomot/papamoscas-deployment-files/papamoscas-proxy-sla/deploy.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 export NODE_ENV=production
 
+sudo apt-get update
+sudo apt-get install -y curl git
+
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y nodejs-legacy
